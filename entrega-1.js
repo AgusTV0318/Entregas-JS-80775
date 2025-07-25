11ed5c7289148c7abf29d2f4848ef75a8e09d804
Agustín <tomasmunizvozza@gmail.com>
2025-07-25 17:48:01 -0300

    entrega 1

---
+++ entrega-1.js
@@ -0,0 +1,23 @@
+let numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+
+console.log(numeros[3]);
+
+let palabras = ["auto", "manzana", "pera", "programacion"];
+
+console.log(palabras);
+
+console.log(palabras[3]);
+
+let boleanos = [true, false, true, false];
+
+console.log(boleanos);
+
+let matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(matriz);
